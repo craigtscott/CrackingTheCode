c1365ad0962ae0a289a8ac5a6f132bf66aca66a2
Craig Scott <craigtscottjr@gmail.com>
2017-10-03 22:11:09 -0700

    1.4.py: takes sting and determined if rearanged it will be a palindrome

--- chapter_1/1.4.py
+++ chapter_1/1.4.py
@@ -3,7 +3,28 @@
 """palindrome permutation: given a string, write a function to check if it is a permutation of a plaindreome. to run program enter `python 1.4.py sting` into the terminal"""
 import sys
 
-def palPer(string):
-    print string
+def palPer(strings):
+    string = "".join(strings)
+    list = [0] * 26
+    oddCount = 0
+    for l in string:
+        index = ord(l.lower())-97
+        if list[index] == 0:
+            list[index] = 1
+            oddCount = oddCount + 1
+        else:
+            list[index] = 0
+            oddCount = oddCount - 1
+            
+    if oddCount > 1:
+        print "False"
+        return False
+
+    print "True"
+    return True
+
 if __name__ == "__main__":
-    palPer(sys.argv[1])
+    palPer(sys.argv[1:])
+
+
+""" Big O(n) determined by the length of string input I decided to make spaces not part of the panendrome"""
