1db4266b75b0bc8fc8feb75932add0532cbe2fcd
Craig Scott <craigtscottjr@gmail.com>
2017-10-03 21:23:23 -0700

    1.4.py inital file

---
+++ chapter_1/1.4.py
@@ -0,0 +1,9 @@
+#! user/bin/env python
+
+"""palindrome permutation: given a string, write a function to check if it is a permutation of a plaindreome. to run program enter `python 1.4.py sting` into the terminal"""
+import sys
+
+def palPer(string):
+    print string
+if __name__ == "__main__":
+    palPer(sys.argv[1])
