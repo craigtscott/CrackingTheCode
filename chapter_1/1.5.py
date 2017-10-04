df85f211d47d31f2b50e861648871e39a296dc01
Craig Scott <craigtscottjr@gmail.com>
2017-10-03 22:16:30 -0700

    1.5.py inital file structure setup

---
+++ chapter_1/1.5.py
@@ -0,0 +1,12 @@
+# !user/bin/src python
+
+""" One Away: There are three types of edits that can be performed on a string: insert a character, remove charater or replace charater. check to see if they are one edit away"""
+import sys
+
+def oneAway(stringA, stringB):
+    print stringA
+    print stringB
+
+
+if __name__ == "__main__":
+    oneAway(sys.argv[1], sys.argv[2])
