5a9ea594e9d9677527810b3821a497a5899af5ee
Craig Scott <craigtscottjr@gmail.com>
2017-10-05 21:54:52 -0700

    1.6 working solution could use refatoring

--- chapter_1/1.6.py
+++ chapter_1/1.6.py
@@ -4,9 +4,24 @@
 import sys
 
 def stringCompression(string):
-    print string
-
+    output = ""
+    tempLetter = string[0]
+    counter = 0
+    for i in range ( 1, len( string)):
+        print i-1
+        print string[i-1]
+        if string[i - 1] == tempLetter:
+            counter = counter + 1
+        if  string[i] != string[i-1]:
+            output = output + string[i-1] + str(counter)
+            counter = 0
+            print output
+            tempLetter = string[i]
+    output = output + tempLetter + str(counter)
+    print output
 
 
 if __name__ == "__main__":
     stringCompression(sys.argv[1])
+
+""" Big 0(n) where n is string length stil could use some refractoring """
