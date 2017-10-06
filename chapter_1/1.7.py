3bab3831cdc170e724c35cf069465bec8e71b886
Craig Scott <craigtscottjr@gmail.com>
2017-10-05 22:01:47 -0700

    1.7.py: set up initial file

---
+++ chapter_1/1.7.py
@@ -0,0 +1,11 @@
+# !usr/bin/src python
+
+""" Rotate Matrix NxN matrix by 90 degrees """
+
+import sys
+
+def rotateMatrix(matrix):
+    print matrix
+
+if __name__ == "__main__":
+    rotateMatrix(sys.argv[1])
