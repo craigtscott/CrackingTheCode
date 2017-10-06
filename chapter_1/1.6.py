286bc5b1dea6ee2bf41d0280996459f5a76dba2b
Craig Scott <craigtscottjr@gmail.com>
2017-10-05 20:50:42 -0700

    1.6: setup of String Compression

---
+++ chapter_1/1.6.py
@@ -0,0 +1,12 @@
+# !usr/bin/src python
+
+""" String Compression: implement a method to perform basic sting compression using counts of repeated charaters. e.g. aabcccccaaa ===> a2b1c5a3 """
+import sys
+
+def stringCompression(string):
+    print string
+
+
+
+if __name__ == "__main__":
+    stringCompression(sys.argv[1])
