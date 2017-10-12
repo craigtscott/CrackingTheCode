97b274a056008ca0273b973bb0591be592a20990
Craig Scott <craigtscottjr@gmail.com>
2017-10-11 20:45:35 -0700

    1.7.py: str into list issue figured out

--- chapter_1/1.5.py
+++ chapter_1/1.5.py
@@ -1,6 +1,8 @@
 # !user/bin/src python
 
 """ One Away: There are three types of edits that can be performed on a string: insert a character, remove charater or replace charater. check to see if they are one edit away"""
+
+""" Still needs work unfinished """
 import sys
 
 def oneAway(stringA, stringB):
--- chapter_1/1.7.py
+++ chapter_1/1.7.py
@@ -3,9 +3,14 @@
 """ Rotate Matrix NxN matrix by 90 degrees """
 
 import sys
+import ast
 
 def rotateMatrix(matrix):
-    print matrix
+    matrix = ast.literal_eval(matrix)
+    
+    if len(matrix) == 0:
+        print False
+
 
 if __name__ == "__main__":
     rotateMatrix(sys.argv[1])
