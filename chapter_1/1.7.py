7bfd3eb2081bfff229af6daac14c582529bf7f4b
Craig Scott <craigtscottjr@gmail.com>
2017-10-11 21:34:07 -0700

    1.7.py: adds inital tests

--- chapter_1/1.7.py
+++ chapter_1/1.7.py
@@ -6,11 +6,18 @@ import sys
 import ast
 
 def rotateMatrix(matrix):
-    matrix = ast.literal_eval(matrix)
     
-    if len(matrix) == 0:
+    if isinstance(matrix, str):
+        matrix = ast.literal_eval(matrix)
+    
+    if ( len(matrix) == 0 or len(matrix) != len(matrix[0])):
         print False
+        return False
 
-
+    print matrix
 if __name__ == "__main__":
-    rotateMatrix(sys.argv[1])
+   # rotateMatrix(sys.argv[1])
+   print "should print: [[3,6,9],[2,5,8],[1,4,7]]"
+   rotateMatrix([[1,2,3],[4,5,6],[7,8,9]])
+   print "should print False"
+   rotateMatrix([[1,2],[1,2],[1,2]])
