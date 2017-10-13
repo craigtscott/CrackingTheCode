8a9b5a3fe997cf405a7e2bc04b823395fa8a458e
Craig Scott <craigtscottjr@gmail.com>
2017-10-12 22:03:33 -0700

    1.8: finish setup add tests

--- chapter_1/1.8.py
+++ chapter_1/1.8.py
@@ -2,8 +2,16 @@
 
 """ Zero Matrix: if a element of a NxM matrix is 0 all in its row and collum is set to 0 """
 import sys
+import ast
+
 def zeroMatrix(matrix):
+    if isinstance(matrix, str):
+        matrix = ast.literal_eval(matrix)
     print matrix
 
 if __name__ == "__main__":
-    zeroMatrix(sys.argv[1])
+    # zeroMatrix(sys.argv[1])
+    print "should be [[0,0],[0,3]]"
+    zeroMatrix([[0,1],[2,3]])
+    print "should be [[1,0,1],[0,0,0][1,0,1]]"
+    zeroMatrix([[1,1,1],[1,0,1],[1,1,1]])
