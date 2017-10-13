026482d09f5b102cc9b453277eaf8a3449cd1535
Craig Scott <craigtscottjr@gmail.com>
2017-10-12 21:56:46 -0700

    1.8.py: builds structure of the file

---
+++ chapter_1/1.8.py
@@ -0,0 +1,9 @@
+# !usr/bin/src python 
+
+""" Zero Matrix: if a element of a NxM matrix is 0 all in its row and collum is set to 0 """
+import sys
+def zeroMatrix(matrix):
+    print matrix
+
+if __name__ == "__main__":
+    zeroMatrix(sys.argv[1])
