dc9479a50d8ae636d83f8c2d4e489e795794c889
Craig Scott <craigtscottjr@gmail.com>
2017-10-13 21:48:46 -0700

    1.8.py: not working quite right but in track

--- chapter_1/1.8.py
+++ chapter_1/1.8.py
@@ -7,8 +7,38 @@ import ast
 def zeroMatrix(matrix):
     if isinstance(matrix, str):
         matrix = ast.literal_eval(matrix)
+    print "initial:" 
+    print matrix
+    row = [False]*len(matrix)
+    col = [False]*len(matrix[0])
+
+    for index in range(0, len(matrix)):
+        for jndex in range(0, len(matrix[0])):
+            if matrix[index][jndex]:
+                row[index] = True
+                col[jndex] = True
+    
+    print row
+    print col
+    for index in range(0, len(row)):
+        if row[index] == True:
+            zeroRow(matrix, index)
+    for index in range(0, len(col)):
+        if col[index] == True:
+            zeroCol(matrix, index)
+
+    print "return:"
     print matrix
 
+def zeroRow(matrix, index):
+    for jndex in range(0, len(matrix[0])):
+        matrix[index][jndex] = 0
+    return matrix
+
+def zeroCol(matrix, jndex):
+    for index in range(0, len(matrix)):
+        matrix[index][jndex] = 0
+    return matrix
 if __name__ == "__main__":
     # zeroMatrix(sys.argv[1])
     print "should be [[0,0],[0,3]]"
