a2b0fba1cd37cbcbc742026f27dc4a2353f44306
Craig Scott <craigtscottjr@gmail.com>
2017-10-17 23:02:28 -0700

    2.3: setup file and initial test

---
+++ chapter_2/2.3.py
@@ -0,0 +1,13 @@
+# !usr/bin/src python
+
+""" Delete Middle Node: Implement an algorythem to delete a node in the middle ex. a>b>c>d>e>f into a>b>d>e>f """
+
+from llist import sllist, sllistnode
+
+def delMidNode( linkedList):
+
+    print linkedList.first
+
+if __name__ == "__main__":
+    ll1 = sllist([1,2,3,4,5])
+    delMidNode(ll1)
