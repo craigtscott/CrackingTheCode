977fae796ac52efbc02ecb3b248e4aa808c84abd
Craig Scott <craigtscottjr@gmail.com>
2017-10-17 23:55:25 -0700

    linkedList adds remove and find but having issues still

--- chapter_2/linkedList.py
+++ chapter_2/linkedList.py
@@ -21,11 +21,36 @@ class linkedList(object):
         new_node = Node(d, self.root)
         self.root = new_node
         self.size += 1
+    def remove(self, d):
+        thisNode = self.root
+        prevNode = Node
+        while thisNode:
+            if thisNode.getData() == d:
+                if prevNode:
+                    prevNode.setNext(thisNode.getNext())
+                else:
+                    self.root = thisNode
+                    self.size -= 1
+                    return True
+            else:
+                prevNode = thisNode
+                thisNode = thisNode.getNext()
 
-
+        return False
+    def find(self, d):
+        thisNode = self.root
+        while thisNode:
+            if thisNode.getData() == d:
+                return d
+            else:
+                thisNode = thisNode.getNext()
+        return Node                
 
 ll1 = linkedList()
 ll1.add(3)
 ll1.add(5)
 ll1.add(7)
-print ll1
+ll1.remove(5)
+print(ll1.remove(7))
+print(ll1.find(5))
+print(ll1.find(3))
