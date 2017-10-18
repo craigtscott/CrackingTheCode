9ea8b941e3940e776f2dbc9594368d6a0aa6e0a8
Craig Scott <craigtscottjr@gmail.com>
2017-10-17 22:54:43 -0700

    2.2.py works well test present

--- chapter_2/2.2.py
+++ chapter_2/2.2.py
@@ -5,11 +5,27 @@
 from llist import sllist, sllistnode
 
 def returnKth(linkedList, k):
-    if len(linkedList) < k:
-        return
-    print "hello"
+
+    pointer1 = linkedList.first
+    pointer2 = linkedList.first
+
+    for index  in range(0, k):
+        if pointer1 == None:
+            return False
+        pointer1 = pointer1.next
+
+    while pointer1 != None:
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next
+    print pointer2
+    return pointer2
 
 if __name__ == "__main__":
     ll1 = sllist([])
+    ll2 = sllist([5,4,3,2,1])
     returnKth(ll1, 4)
-    returnKth(ll1, 0)
+    returnKth(ll2, 3)
+    returnKth(ll2, 4)
+    returnKth(ll2, 5)
+    returnKth(ll2, 6)
+    returnKth(ll2, 0)
