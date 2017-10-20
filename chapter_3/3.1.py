e1a573c9736b20ee268f5f6499f9bae04e043a61
Craig Scott <craigtscottjr@gmail.com>
2017-10-19 23:36:52 -0700

    3.1 pop in progress

--- chapter_3/3.1.py
+++ chapter_3/3.1.py
@@ -18,7 +18,15 @@ class ThreeStacks(object):
             self.values[self.indexHash[stackNum][0]] = value
             self.indexHash[stackNum] = [self.indexHash[stackNum][0] + 1, self.indexHash[stackNum][1]]
             return True
-
+    def pop(self, stackNum):
+        if self.indexHash[stackNum][1] - self.indexHash[stackNum][0] == self.stackSize:
+            print "empty"
+            return False
+        else:
+            print self.indexHash[stackNum][0]            
+            self.values[self.indexHash[stackNum][0]] = None
+            self.indexHash[stackNum] = [self.indexHash[stackNum][0] - 1, self.indexHash[stackNum][1]]
+            print self.indexHash[stackNum]
     def printStacks(self):
         print self.values
         return True
@@ -28,5 +36,5 @@ multi = ThreeStacks(3)
 multi.push(0,1)
 multi.push(0,2)
 multi.push(0,3)
-multi.push(0,4)
+multi.pop(0)
 multi.printStacks()
