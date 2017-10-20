3fbb8de1c8ed55759aaf36ffcfe2d8978feab872
Craig Scott <craigtscottjr@gmail.com>
2017-10-19 23:45:59 -0700

    3.1 pop is functional

--- chapter_3/3.1.py
+++ chapter_3/3.1.py
@@ -7,7 +7,7 @@ class ThreeStacks(object):
         self.values = [None] * stackSize * 3
         self.stackSize = stackSize
 
-        self.indexHash = {0: [0, self.stackSize], 1: [self.stackSize, self.stackSize * 2], 2: [self.stackSize * 2, self.stackSize * 3]}
+        self.indexHash = {0: [0, self.stackSize], 1: [self.stackSize, self.stackSize * 2 ], 2: [self.stackSize * 2, self.stackSize * 3]}
 
     def push(self, stackNum, value):
         print self.indexHash
@@ -24,7 +24,7 @@ class ThreeStacks(object):
             return False
         else:
             print self.indexHash[stackNum][0]            
-            self.values[self.indexHash[stackNum][0]] = None
+            self.values[self.indexHash[stackNum][0] - 1] = None
             self.indexHash[stackNum] = [self.indexHash[stackNum][0] - 1, self.indexHash[stackNum][1]]
             print self.indexHash[stackNum]
     def printStacks(self):
@@ -37,4 +37,7 @@ multi.push(0,1)
 multi.push(0,2)
 multi.push(0,3)
 multi.pop(0)
+multi.push(1,4)
+multi.printStacks()
+multi.pop(1)
 multi.printStacks()
