3fdb1a03eb1a8e083a1b7b98b9186e9dfe861eea
Craig Scott <craigtscottjr@gmail.com>
2017-10-24 21:16:25 -0700

    3.2: push is working but needs adjustments

--- chapter_3/3.2.py
+++ chapter_3/3.2.py
@@ -3,19 +3,24 @@
 """Stack Min: How would you design a stack which, in addition to push a pop, has a function min which returns the  minimum element? Push, pop and min should all operate in O(1) time. """
 
 from ThreeStacks import ThreeStacks
-class StackWithMin(ThreeStacks):
-    def __init__(self, stackSize):
-        self.minimum = [None]*3
-        self.stacks = ThreeStacks.__init__(self, stackSize)
-
-    def push(self, stackNum, value):
-        if self.minimum[stackNum] > value:
-            self.minimum[stackNum] = value
-        self.stacks.push(stackNum, value)
-        print  self.minimum[stackNum]
+class StackWithMin(object):
+    def __init__(self):
+        self.minimum = None
+        self.values = [None]*10
+        self.index = 0
 
 
+    def push(self, value):
+        if self.minimum > value or self.minimum == None:
+            self.minimum = value
+        
+        self.values[self.index] = value
+        self.index = self.index + 1
+        print  self.minimum
+        print self.values
 
-min = StackWithMin(ThreeStacks(9))
-min.push(0,5)
-min.push(0,4)
+
+
+min = StackWithMin()
+min.push(5)
+min.push(4)
